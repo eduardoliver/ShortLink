296c2725ac165b2c0696d76b77e879d873e6105d
Eduard Oliver <eduardoliver@gmail.com>
2021-05-08 22:39:23 +0100

    minor

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,8 +3,7 @@ class LinksController < ApplicationController
   def create
     shortener = Shortener.new(link_params[:original_url])
     @link = shortener.generate_short_link
-
-    
+    byebug
   end
 
 
