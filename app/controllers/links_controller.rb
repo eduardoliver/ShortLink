bde6c2b420eaa05d99845e9512c426052c948d2d
Eduard Oliver <eduardoliver@gmail.com>
2021-05-08 21:59:49 +0100

    FIRST simple example of controller rspec testing

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,3 +1,8 @@
 class LinksController < ApplicationController
 
+  def create
+    original_url = params[:link][:original_url]
+    shortener = Shortener.new(original_url)
+    @link = shortener.generate_short_link
+  end
 end
